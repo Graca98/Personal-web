c8f884ba3f3c6f3b436cc41ec3711d74123f9407
Grácouš <gracous1234@gmail.com>
2024-09-02 21:23:03 +0200

    Struktura stránky, background, základní styly

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,127 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
-      <section className="max-w-2xl mx-auto">
-        <h1>Denis Gracias</h1>
-        <h2>Junior Frontend Developer</h2>
-        <h3>Zde bude krátký popis o tom co vytvářím</h3>
-      </section>
-    </main>
+    <div className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-20 lg:px-24 lg:py-0">
+      <div className="lg:flex lg:justify-between lg:gap-4">
+        <header className="lg:max-h-screen w-1/2 lg:top-0 py-24 lg:sticky">
+          <section className="">
+            <h1 className="text-4xl font-bold mb-3">Denis Gracias</h1>
+            <h2 className="text-xl font-semibold mb-3">
+              Junior Frontend Developer
+            </h2>
+            <h3 className="leading-normal">
+              Zde bude krátký popis o tom co vytvářím a tak dále...
+            </h3>
+          </section>
+        </header>
+
+        <main className="w-1/2 py-24">
+          <section id="aboutme" className="mb-24">
+            <div></div>
+            <p className="mb-4 text-slate-400 leading-normal">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
+              quam excepturi repellat modi consectetur. Ratione similique
+              molestias reiciendis ducimus unde fuga quasi. Odit facere et,
+              maxime error obcaecati tempore saepe? Lorem, ipsum dolor sit amet
+              consectetur adipisicing elit. Dolorum ut similique quasi facilis
+              eaque aliquam dignissimos corporis consequuntur tempora
+              necessitatibus. Nemo nihil nobis quo repudiandae facilis quod,
+              quidem soluta dolore.
+            </p>
+            <p className="mb-4 text-slate-400 leading-relaxed">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Distinctio, totam illo animi laudantium eius laborum earum dolore
+              magni, ipsa delectus esse rem ullam corrupti repellendus nulla
+              nisi. Esse, hic nobis. Lorem ipsum, dolor sit amet consectetur
+              adipisicing elit. In modi animi blanditiis aliquid dolorem eius
+              excepturi quis, iusto labore inventore similique voluptates a
+              sequi vero nostrum accusantium rerum provident sapiente!
+            </p>
+            <p className="mb-4">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima a
+              amet magni ullam perspiciatis, doloremque quisquam commodi veniam
+              ipsa, obcaecati eaque aut fugiat alias! Ipsum eum illum molestiae
+              ducimus aperiam.
+            </p>
+          </section>
+          <section id="experience" className="mb-24">
+            <h2 className="font-bold text-2xl mb-4">Moje zkušenosti</h2>
+            <div>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae
+              quia vitae magni ullam numquam amet, dignissimos nemo! Doloribus,
+              veritatis. Magnam fugit molestias molestiae impedit autem
+              repellendus omnis sapiente dolorem accusamus! Lorem, ipsum dolor
+              sit amet consectetur adipisicing elit. Quo totam vel reprehenderit
+              aliquid qui? Sunt repellendus excepturi officia ullam ex ea
+              corporis temporibus consequuntur libero consequatur, ad, facere
+              alias. Itaque? Lorem, ipsum dolor sit amet consectetur adipisicing
+              elit. Minima facere cum tempora rem, placeat doloribus
+              exercitationem culpa atque provident quo fugiat? Adipisci eos
+              aliquid ratione quam eum laboriosam rem quos! Lorem ipsum dolor
+              sit, amet consectetur adipisicing elit. Totam, optio
+              exercitationem modi eaque perferendis similique nihil, neque
+              repellendus veniam maxime voluptate. Eum, odit ratione. Doloribus
+              aut quod saepe ratione rem! Lorem ipsum dolor sit, amet
+              consectetur adipisicing elit. Aliquam tempore, dolorum fugit
+              beatae aspernatur obcaecati tempora? Eaque minus veritatis
+              consequatur dignissimos quasi, doloribus modi odio exercitationem
+              mollitia quia. Fuga, sed?
+            </div>
+            <div>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae
+              quia vitae magni ullam numquam amet, dignissimos nemo! Doloribus,
+              veritatis. Magnam fugit molestias molestiae impedit autem
+              repellendus omnis sapiente dolorem accusamus! Lorem, ipsum dolor
+              sit amet consectetur adipisicing elit. Quo totam vel reprehenderit
+              aliquid qui? Sunt repellendus excepturi officia ullam ex ea
+              corporis temporibus consequuntur libero consequatur, ad, facere
+              alias. Itaque? Lorem, ipsum dolor sit amet consectetur adipisicing
+              elit. Minima facere cum tempora rem, placeat doloribus
+              exercitationem culpa atque provident quo fugiat? Adipisci eos
+              aliquid ratione quam eum laboriosam rem quos! Lorem ipsum dolor
+              sit, amet consectetur adipisicing elit. Totam, optio
+              exercitationem modi eaque perferendis similique nihil, neque
+              repellendus veniam maxime voluptate. Eum, odit ratione. Doloribus
+              aut quod saepe ratione rem! Lorem ipsum dolor sit, amet
+              consectetur adipisicing elit. Aliquam tempore, dolorum fugit
+              beatae aspernatur obcaecati tempora? Eaque minus veritatis
+              consequatur dignissimos quasi, doloribus modi odio exercitationem
+              mollitia quia. Fuga, sed?
+            </div>
+            <div>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae
+              quia vitae magni ullam numquam amet, dignissimos nemo! Doloribus,
+              veritatis. Magnam fugit molestias molestiae impedit autem
+              repellendus omnis sapiente dolorem accusamus! Lorem, ipsum dolor
+              sit amet consectetur adipisicing elit. Quo totam vel reprehenderit
+              aliquid qui? Sunt repellendus excepturi officia ullam ex ea
+              corporis temporibus consequuntur libero consequatur, ad, facere
+              alias. Itaque? Lorem, ipsum dolor sit amet consectetur adipisicing
+              elit. Minima facere cum tempora rem, placeat doloribus
+              exercitationem culpa atque provident quo fugiat? Adipisci eos
+              aliquid ratione quam eum laboriosam rem quos! Lorem ipsum dolor
+              sit, amet consectetur adipisicing elit. Totam, optio
+              exercitationem modi eaque perferendis similique nihil, neque
+              repellendus veniam maxime voluptate. Eum, odit ratione. Doloribus
+              aut quod saepe ratione rem! Lorem ipsum dolor sit, amet
+              consectetur adipisicing elit. Aliquam tempore, dolorum fugit
+              beatae aspernatur obcaecati tempora? Eaque minus veritatis
+              consequatur dignissimos quasi, doloribus modi odio exercitationem
+              mollitia quia. Fuga, sed?
+            </div>
+          </section>
+          <section id="projects" className="mb-24">
+            <h2 className="font-bold text-2xl mb-4">Projekty</h2>
+            <div>Proejkt 1</div>
+            <div>Proejkt 2</div>
+            <div>Proejkt 3</div>
+            <div>Proejkt 4</div>
+            <div>Proejkt 5</div>
+            <div>Proejkt 6</div>
+          </section>
+          <footer></footer>
+        </main>
+      </div>
+    </div>
   );
 }
